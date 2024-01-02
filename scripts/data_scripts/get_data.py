4a2049a4a7157c963366f5ad5680fbe85f3df4ad
Lemeri02 <9588504@gmail.com>
2024-01-02 12:38:32 +0000

    add datasets download script

---
+++ scripts/data_scripts/get_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+
+url_train = 'https://raw.githubusercontent.com/Lemeri02/mlops_practice_2_data/main/train.csv'
+url_test = 'https://raw.githubusercontent.com/Lemeri02/mlops_practice_2_data/main/test.csv'
+train_df = pd.read_csv(url_train)
+test_df = pd.read_csv(url_test)
+
+
+train_df.to_csv('../../data/raw/train.csv', sep=',', index = False)
+test_df.to_csv('../../data/raw/test.csv', sep=',', index = False)
